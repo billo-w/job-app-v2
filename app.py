f2eac13cb28246802e5dfdf10fed3ebb10d65653
billo1w <belalwaw@gmail.com>
2025-04-29 10:17:17 +0000

    fix db error

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ AZURE_AI_KEY = os.getenv('AZURE_AI_KEY')
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     saved_jobs = db.relationship('SavedJob', backref='user', lazy=True, cascade="all, delete-orphan") # Relationship
 
     def set_password(self, password):
